b1a0b44a21c85461ebf8fb102f479e273ada11e2
caraujo07 <caraujo@jc.com.br>
2019-08-01 16:32:07 -0300

    comentando componente  Hello world

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <SearchBar />
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
