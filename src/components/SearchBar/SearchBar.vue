aba0d73c08c4433f08805bb95d4b4579f692115a
caraujo07 <caraujo@jc.com.br>
2019-08-02 17:50:11 -0300

    alterações

--- src/components/SearchBar/SearchBar.vue
+++ src/components/SearchBar/SearchBar.vue
@@ -1,22 +1,19 @@
 <template>
   <section id="search-bar">
-    <input type="text" v-model="busca" placeholder="Busque um vídeo" @input="getVideo()">
+    <input type="text" v-model="busca" placeholder="Busque um vídeo">
     <div v-if="loaded">
      
-      <div v-for="res in resultado" :key="res.itag">
-        
-        <iframe v-for="(id, index) in videoId" :src="id" :key="index" frameborder="0"></iframe>
-        
-        
-      </div>
+      <ul>
+        <li v-for="(valor, chave, index) in videoId" :key="index">
+          <h1>{{valor.video.title}}</h1>
+          <iframe width="560" height="315" :src="valor.video.link" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </li>
+      </ul>
 
     </div>
-    <button @click=getVideo>buscar vídeo</button>
+    <button @click="getVideo">buscar vídeo</button>
   </section>
 </template>
-
-
-
 
 <script>
 export default {
@@ -26,14 +23,14 @@ export default {
       videos: {},
       busca: '',
       resultado: [],
-      videoId: [], 
+      videoId: [],
       loaded: false
     }
   },
   methods: {
     getVideo() {
 
-      const url = (`https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCEmAseUxKNNvU_2M3ip8zYWqSFYXLvvPs&q=${this.busca}&maxResults=10&type=video`);
+      const url = (`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=AIzaSyCEmAseUxKNNvU_2M3ip8zYWqSFYXLvvPs&playlistId=PLHz_AreHm4dksnH2jVTIVNviIMBVYyFnH&maxResults=50`);
 
       // let params = {
       //   part: 'snippet',
@@ -41,6 +38,7 @@ export default {
       //   q: this.busca,
       //   maxResults: 10,
       //   type: 'video'
+      //   playlistid curso em video python = PLHz_AreHm4dksnH2jVTIVNviIMBVYyFnH
       // };
 
       fetch(url)
@@ -48,7 +46,9 @@ export default {
       .then(r => {
         this.videos = r;
         this.resultado = r.items;
-        this.videoId = r.items.id.videoId;
+        this.resultado.forEach((item) => {
+          this.videoId.push({"video": {"title": item.snippet.title, "link":"https://www.youtube.com/embed/" + item.snippet.resourceId.videoId}});
+        });
         this.loaded = true;
       });
 
